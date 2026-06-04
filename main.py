176e1cefd3b0e51ba2582fdd1e5f05ec74a32fb8
gastonalvez18 <GASTONALVEZ18@HOTMAIL.COM>
2026-06-04 16:56:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,19 +13,6 @@ def home():
     }
 
 
-@app.get("/iphone")
-def buscar_iphone():
-
-    url = "https://api.mercadolibre.com/sites/MLU/search?q=iphone"
-
-    r = requests.get(url)
-
-    return {
-        "status_code": r.status_code,
-        "respuesta": r.json()
-    }
-
-
 @app.get("/test")
 def test():
     return {
@@ -33,18 +20,25 @@ def test():
     }
 
 
-@app.get("/test2")
-def test2():
+@app.get("/iphone")
+def buscar_iphone():
 
     headers = {
         "User-Agent": "Mozilla/5.0"
     }
 
-    url = "https://www.mercadolibre.com.uy"
+    url = "https://listado.mercadolibre.com.uy/iphone"
 
     r = requests.get(url, headers=headers)
 
+    soup = BeautifulSoup(r.text, "lxml")
+
+    titulos = []
+
+    for h3 in soup.find_all("h3")[:20]:
+        titulos.append(h3.get_text())
+
     return {
-        "status_code": r.status_code,
-        "largo_html": len(r.text)
+        "cantidad": len(titulos),
+        "titulos": titulos
     }
