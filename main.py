206c12a8e74935c845980c27dbe07c78e9412a9c
gastonalvez18 <GASTONALVEZ18@HOTMAIL.COM>
2026-06-04 17:43:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,15 @@ def playwright_test():
 
         with sync_playwright() as p:
 
-            browser = p.chromium.launch(headless=True)
+            browser = p.chromium.launch(
+                headless=True,
+                args=[
+                    "--no-sandbox",
+                    "--disable-setuid-sandbox",
+                    "--disable-dev-shm-usage",
+                    "--disable-gpu"
+                ]
+            )
 
             page = browser.new_page()
 
