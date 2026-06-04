22d74b9dee0ca0cf28571548fb83ea22fa16bc12
gastonalvez18 <GASTONALVEZ18@HOTMAIL.COM>
2026-06-04 16:01:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI(title="Radar de Oportunidades")
+
 
 @app.get("/")
 def home():
@@ -8,3 +10,25 @@ def home():
         "status": "online",
         "project": "Radar de Oportunidades"
     }
+
+
+@app.get("/iphone")
+def buscar_iphone():
+
+    url = "https://api.mercadolibre.com/sites/MLU/search?q=iphone"
+
+    r = requests.get(url)
+
+    data = r.json()
+
+    resultados = []
+
+    for item in data.get("results", [])[:20]:
+
+        resultados.append({
+            "titulo": item.get("title"),
+            "precio": item.get("price"),
+            "link": item.get("permalink")
+        })
+
+    return resultados
