c671985d06eb2fb8e3ff232dd9aa783ce298b3a3
gastonalvez18 <GASTONALVEZ18@HOTMAIL.COM>
2026-06-04 16:57:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,23 @@ def test():
     }
 
 
+@app.get("/test2")
+def test2():
+
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    url = "https://www.mercadolibre.com.uy"
+
+    r = requests.get(url, headers=headers)
+
+    return {
+        "status_code": r.status_code,
+        "largo_html": len(r.text)
+    }
+
+
 @app.get("/iphone")
 def buscar_iphone():
 
